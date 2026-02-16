7b22966e886d513a300c7fa6989633ba08a37813
Muhammad Ikram <mikrams0663@gmail.com>
2026-02-16 17:33:04 +0500

    Billing update & Profile page code update

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,20 +1,50 @@
-import CompanionForm from "@/components/CompanionForm"
-import { auth } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import CompanionForm from "@/components/CompanionForm";
+import { newCompanionsPermissions } from "@/lib/actions/companion.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 const NewCompanion = async () => {
   const { userId } = await auth();
-  if(!userId){
-    redirect('/sign-in');
+
+  if (!userId) {
+    redirect("/sign-in");
   }
+
+  const canCreateCompanion = await newCompanionsPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
-        <CompanionForm />
-      </article>
+      {canCreateCompanion ? (
+        <article className="w-full gap-4 flex flex-col">
+          <h1>Companion Builder</h1>
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src="/images/limit.svg"
+            alt="Companion Limit Reached"
+            width={360}
+            height={230}
+          />
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You&apos;ve Reached Your Limit</h1>
+          <p>
+            You&apos;ve reached your companion limit. Upgrade to create more
+            companions and access premium features.
+          </p>
+          <Link
+            href="/subscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade My Plan
+          </Link>
+        </article>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default NewCompanion
+export default NewCompanion;
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
+ 
 
 export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth();
@@ -91,7 +92,55 @@ export const getUserSessions = async (userId: string, limit = 10) => {
   .limit(limit);
 
   if(error) throw new Error(error.message);
-
   return data.map(({companions}) => companions
-  )
+  );
 };
+
+
+export const getUserCompanions = async (userId: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error} =await supabase
+  .from('companions')
+  .select()
+  .eq('author', userId)  
+  
+
+  if(error) throw new Error(error.message);
+  return data;
+  
+};
+
+
+export const newCompanionsPermissions = async () => {
+      const { userId, has } = await auth();
+      const supabase = createSupabaseClient();
+
+      let limit = 0;
+      if(has({ plan: 'pro '})){
+        return true;
+
+      } else if (has ({ feature: "3_companion_limit"})){
+        limit = 3;
+
+      }
+      else if (has ({ feature: "10_companion_limit" })){
+        limit = 10;
+      }
+      const { data, error } = await supabase
+      .from('companions')
+      .select('id')
+      .eq('author', userId)
+
+      if(error) throw new Error(error.message);
+      const companionCount = data ?.length;
+      if(companionCount >= limit){
+        return false
+
+      }
+      else {
+        return true;
+      }
+
+}
+
+
